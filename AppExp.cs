17e5970617d38cdc4b9b91a4d83a03b95a4f9d17
nikitha31 <59816775+nikitha31@users.noreply.github.com>
2020-02-03 10:26:45 +0530

    Add files via upload

---
+++ AppExp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace AppExp
+{
+    class AppExp
+    {
+        public void Divide(int a, int b)
+        {
+
+
+            int result = a / b;
+            Console.WriteLine(result);
+
+
+        }
+        /// <summary>
+        /// Add Function with Overflow Excedption. 
+        /// </summary>
+        public void Sub(int a, int b)
+        {
+
+            int result = a - b;
+
+            Console.WriteLine(result);
+
+        }
+        public void Add(int a, int b)
+        {
+
+
+            int result = a + b;
+            Console.WriteLine(result);
+
+        }
+
+        /// <summary>
+        /// Multiplication Function with Overflow Excedption. 
+        /// </summary>
+        public void Mul(int a, int b)
+        {
+
+            int result = a * b;
+            Console.WriteLine(result);
+
+
+        }
+        static void Main(string[] args)
+        {
+            //System Define Exception.
+            AppExp appException = new AppExp();
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine("enter 2 nums");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if ((x % 2 != 0) || (y % 2 != 0))
+                {
+                    throw new NotEvenException();
+                }
+                appException.Divide(x, y);
+                appException.Sub(x, y);
+                appException.Add(x, y);
+                appException.Mul(x, y);
+
+            }
+            catch (NotEvenException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //appException.Divide(x,y);
+            //appException.Sub(x,y);
+            //appException.Add(x,y);
+            //appException.Mul(x,y);
+
+
+        }
+    }
+    public class NotEvenException : ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "not even number";
+            }
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace SysException
+{
+    class Program
+    {
+            /// <summary>
+            /// Divide Function with DivideByZeroException Excedption. 
+            /// </summary>
+            public void Divide()
+            {
+                try
+                {
+                    Console.WriteLine("Enter  2 numbers");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine()); ;
+                    int result = a / b;
+                    Console.WriteLine(result);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+            /// <summary>
+            /// Add Function with Overflow Excedption. 
+            /// </summary>
+        public void Sub()
+        {
+            try
+            {
+                Console.WriteLine("Enter any 2 numbers");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine()); ;
+                int result;
+                checked
+                {
+                    result = a - b;
+                    Console.WriteLine(result);
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+        }
+        public void Add()
+            {
+                try
+                {
+                    Console.WriteLine("Enter any 2 numbers");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine()); ;
+                    int result;
+                    checked
+                    {
+                        result = a + b;
+                        Console.WriteLine(result);
+                    }
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    
+                }
+            }
+
+            /// <summary>
+            /// Multiplication Function with Overflow Excedption. 
+            /// </summary>
+            public void Mul()
+            {
+                try
+                {
+                    Console.WriteLine("Enter any 2 numbers");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine()); ;
+                    int result;
+                    checked
+                    {
+                        result = a * b;
+                        Console.WriteLine(result);
+                    }
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    
+                }
+            }
+            static void Main(string[] args)
+        {
+            //System Define Exception.
+            Program systemException = new Program();
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            systemException.Divide();
+            systemException.Sub();
+            systemException.Add();
+            systemException.Mul();
+
+            
+        }
+    }
+
+      
+}
